bc15058fcdc14b56bc3edafd1aa1e64b9eddfe4d
kodqalam <qalam@live.com.my>
2024-12-25 17:45:35 +0800

    nota3

--- index.php
+++ index.php
@@ -7,5 +7,13 @@
 	<p>hello to github to server</p>
 	
 	<h4>development sistem</h4>
+
+
+	<ul>
+		<li>Learn github the basic</li>
+		<li>Make project</li>
+		<li>Push to staging/prod</li>
+
+	</ul>
 	
 </html>
