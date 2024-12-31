cd8177f18a11d67bc08fa12460c9937915bf6467
kodqalam <qalam@live.com.my>
2024-12-31 22:28:55 +0800

    color

---
+++ header.php
@@ -0,0 +1,9 @@
+
+
+
+
+	<ul class="menu">
+		<li><a href="">Home</a></li>
+		<li><a href="">About</a></li>
+		<li><a href="">Blogs</a></li>
+	</ul>
--- index.php
+++ index.php
@@ -1,19 +1,52 @@
-
-
-
-
 <html>
+<head>
+<?php include "header.php";?>
+<html>
+<title>Main</title>
 	
-	<p>hello to github to server</p>
-	
-	<h4>development sistem</h4>
+<style>
 
+body{
+background-color: grey;
+}
+
+ul.menu li {
+  display : inline;
+  color : red;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+
+
+</style>
+</head>
+
+
+<body>
 
 	<ul>
-		<li>Learn github the basic</li>
-		<li>Make project</li>
-		<li>Push to staging/prod</li>
-
+	<li>Learn github the basic</li>
+	<li>Make project</li>
+	<li>Push to staging/prod</li>
 	</ul>
+
+
+</body>
 	
 </html>
